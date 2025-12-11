a0eb91b302b6c01ea4bd7f6b8fa763d0fad3cbd9
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2025-12-11 10:35:51 +0200

    Update Script.cmd

--- Script.cmd
+++ Script.cmd
@@ -7,4 +7,6 @@ REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /f /t
 REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassNRO /f /t REG_DWORD /d 00000001
 REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v OOBEBypassNRO /f /t REG_DWORD /d 00000001
 REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassMSARequirement /f /t REG_DWORD /d 00000001
+REG ADD HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /f /t REG_DWORD /d 00000001
+
 echo Bybass completed, you are good to go!
